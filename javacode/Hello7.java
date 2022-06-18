1f30dd69ade8edfa4683b8a36040b76618a139e9
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-06-18 14:23:22 +0800

    Add files via upload

--- javacode/Hello7.java
+++ javacode/Hello7.java
@@ -1,3 +1,8 @@
+/*
+Java 11 支援直接執行 .java 程式
+語法: java Hello7.java 
+注意: 若 Hello7.class 已經存在則不可以使用上述方法
+*/
 public class Hello7 {
 	public static void main(String[] args) {
 		System.out.println("I love java");

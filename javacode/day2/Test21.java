16f281bb3449f5f8e06dc4524d1c8ec736e94422
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-06-25 16:30:00 +0800

    Add files via upload

--- javacode/day2/Test21.java
+++ javacode/day2/Test21.java
@@ -5,7 +5,7 @@ import java.util.Random; // 隨機數
 public class Test21 {
 	public static void main(String[] args) {
 		Random random = new Random();
-
+		// 按下 Ctrl + C 可以跳離迴圈
 		while(true) {
 			System.out.println(random.nextInt()); // 隨機整數
 			System.out.println(random.nextInt(10)); // 0~9 的隨機整數

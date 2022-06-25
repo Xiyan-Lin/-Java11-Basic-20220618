162207981a8e3064930a23549272ab99f804ee8c
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-06-25 14:24:00 +0800

    Add files via upload

---
+++ javacode/day2/Test13.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class Test13 {
+	public static void main(String[] args) {
+		// 停車資訊
+		int pos = 0b101101;
+		System.out.println(pos);
+		System.out.println(Integer.toBinaryString(pos));
+		// 將 no 號的車子移開
+		int no = 3;
+		int car = (int)Math.pow(2, no);
+		if((pos & car) > 0) {
+			System.out.printf("%d 車位可以移車\n", no);
+			// 進行移車
+			pos = pos - car;
+			System.out.println(Integer.toBinaryString(pos));
+			System.out.println(pos);
+		} else {
+			System.out.printf("%d 車位無車\n", no);
+		}
+	}
+}

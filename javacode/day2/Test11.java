2ecac3b71812893cf55de306a163c905edbcd20a
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-06-25 13:44:30 +0800

    Add files via upload

---
+++ javacode/day2/Test11.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class Test11 {
+	public static void main(String[] args) {
+		int x = 5 & 3;
+		System.out.println(x);
+		int y = 5 | 3;
+		System.out.println(y);
+		int z = 5 ^ 3;
+		System.out.println(z);
+			
+	}
+}

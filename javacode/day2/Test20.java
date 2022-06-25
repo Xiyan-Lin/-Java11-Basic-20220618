f7d1ff23a0977d8bc8d07fcea0dd9e8d9f7c4428
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-06-25 16:19:12 +0800

    Add files via upload

--- javacode/day2/Test20.java
+++ javacode/day2/Test20.java
@@ -15,20 +15,14 @@ public class Test20 {
 		// 改寫使用 switch-case
 		switch(code) {
 			case "A":
-				System.out.println("台北市");
-				break;
 			case "10":
 				System.out.println("台北市");
 				break;	
 			case "F":
-				System.out.println("新北市");
-				break;	
 			case "15":
 				System.out.println("新北市");
 				break;		
 			case "H":
-				System.out.println("桃園市");
-				break;	
 			case "17":
 				System.out.println("桃園市");
 				break;		

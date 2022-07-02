274f712138d8a24fd4b50153620afd687999c029
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-07-02 11:49:19 +0800

    Add files via upload

--- javacode/day3/Loop6.java
+++ javacode/day3/Loop6.java
@@ -4,11 +4,14 @@ package day3;
 // 九九乘法表
 public class Loop6 {
 	public static void main(String[] args) {
-		int x = 1;
-		for(int y = 1 ; y <= 9 ; y++) {
-			System.out.printf("%dx%d=%d ", x, y, (x*y));
-		}
-		System.out.println();
+		for(int x = 1 ; x <= 9 ; x++) {
 
+			for(int y = 1 ; y <= 9 ; y++) {
+				System.out.printf("%dx%d=%d ", x, y, (x*y));
+			}
+			System.out.println();
+
+		}
+		
 	}
 }

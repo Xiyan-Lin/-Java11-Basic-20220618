90b65f0a1a41c2b619f062a182ddd7951d8a5297
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-07-02 12:06:22 +0800

    Add files via upload

--- javacode/day3/Loop6.java
+++ javacode/day3/Loop6.java
@@ -7,7 +7,7 @@ public class Loop6 {
 		for(int x = 1 ; x <= 9 ; x++) {
 
 			for(int y = 1 ; y <= 9 ; y++) {
-				System.out.printf("%dx%d=%d ", x, y, (x*y));
+				System.out.printf("%2dx%2d=%2d ", x, y, (x*y));
 			}
 			System.out.println();
 
